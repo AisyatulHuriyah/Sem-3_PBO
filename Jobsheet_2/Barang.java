44718ecb4fe91d7199884cb8b2fde71bb8fb1aba
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2026-09-01 19:31:43 +0700

    Experiment 2

---
+++ Jobsheet_2/Barang.java
@@ -0,0 +1,20 @@
+package Jobsheet_2;
+
+public class Barang {
+    public String code;
+    public String name;
+    public double grossPrice;
+    public double discount;
+
+    public double netPrice() {
+        return grossPrice - discount * grossPrice;
+    }
+
+    public void displayInfo() {
+        System.out.println("Code       : " + code);
+        System.out.println("Name       : " + name);
+        System.out.println("Gross Price: " + grossPrice);
+        System.out.println("Discount   : " + discount);
+        System.out.println("Net Price  : " + netPrice());
+    }
+}
